8e2a764d33fe3eebf52257047a38974adf5cedac
Ângelo <hoanngelos@gmail.com>
2022-09-20 17:01:02 -0300

    created a context for products.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.scss';
 import App from './App';
 import { UserProvider } from './contexts/user.context';
-import reportWebVitals from './reportWebVitals';
+import { ProductsProvider } from './contexts/products.context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -12,10 +12,10 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App/>
+        <ProductsProvider>
+          <App/>
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
-
-reportWebVitals();
