baf27f0416f92e465e312f7c65059196dad4ff4f
Ângelo <hoanngelos@gmail.com>
2022-09-24 20:37:00 -0300

    cart dropdown now is working.

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -3,6 +3,7 @@ import { Outlet, Link } from 'react-router-dom';
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 import { UserContext } from '../../contexts/user.context';
+import { CartContext } from '../../contexts/cart.context';
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 
@@ -10,6 +11,7 @@ import './navigation.styles.scss';
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
 
   return (
     <Fragment>
@@ -33,7 +35,7 @@ const Navigation = () => {
           )}
           <CartIcon/>
         </div>
-        <CartDropdown/>
+        {isCartOpen && <CartDropdown/>}
       </div>
       <Outlet />
     </Fragment>
