d3d06bc634fcea0563a313002b9ff25c95e1c92c
Ângelo <hoanngelos@gmail.com>
2022-09-24 20:13:26 -0300

    cart dropdown added to the project.

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,8 +1,8 @@
 import { Fragment, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 import { UserContext } from '../../contexts/user.context';
-
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 
@@ -33,6 +33,7 @@ const Navigation = () => {
           )}
           <CartIcon/>
         </div>
+        <CartDropdown/>
       </div>
       <Outlet />
     </Fragment>
