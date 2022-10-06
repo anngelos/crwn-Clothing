61c6f1edf85f9954ba1760281bd6699be67d1f8d
Ângelo <hoanngelos@gmail.com>
2022-10-06 13:58:48 -0300

    changes done

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,22 +1,23 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useState, useEffect } from "react";
 
-import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils";
 
 export const CategoriesContext = createContext({
   categoriesMap: {},
 });
 
 export const CategoriesProvider = ({ children }) => {
-  const [categoriesMap, setCategoriesMap] = useState({});
+  const [categoriesMap, setCategoriesMap] = useState();
 
   useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments('categories');
+    getCategoriesAndDocuments("categories").then((categoryMap) => {
       setCategoriesMap(categoryMap);
-    };
-
-    getCategoriesMap();
+    });
   }, []);
+
+  if (!categoriesMap) {
+    return <div>carregando...</div>;
+  }
 
   const value = { categoriesMap };
   return (
