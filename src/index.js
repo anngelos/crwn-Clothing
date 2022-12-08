c47a6f96ab0dedb3e2b7bb4940b9d3af9f108b26
Ângelo <83850175+anngelos@users.noreply.github.com>
2022-12-08 03:55:08 -0300

    redux persist added to the project.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./store/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./store/store";
 import "./index.scss";
 import App from "./App";
 
@@ -10,10 +11,12 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <Provider store={store} >
-      <BrowserRouter>
-              <App />
-      </BrowserRouter>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
