87ac46cb6c42f4a04f1e3a34a48d3258fcd95b45
Ângelo <83850175+anngelos@users.noreply.github.com>
2022-12-14 16:32:00 -0300

    spinner added to loading clothes pages and categories.

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,30 +1,37 @@
 import { useState, useEffect, Fragment } from "react";
-import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 import ProductCard from "../../components/product-card/product-card.component";
-import { selectCategoriesMap } from "../../store/categories/categories.selector";
-import "./category.styles.scss";
+import Spinner from "../../components/spinner/spinner.component";
+import { selectCategoriesMap, selectIsLoading } from "../../store/categories/categories.selector";
+import { CategoryContainer, Title } from "./category.styles";
+
 
 const Category = () => {
   const { category } = useParams();
   const categoriesMap = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectIsLoading);
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
     setProducts(categoriesMap[category]);
-  }, [category, categoriesMap])
+  }, [category, categoriesMap]);
 
   return (
     <Fragment>
-      <h2 className="category-title">{category.toUpperCase()}</h2>
-      <div className="category-container">
-        {
-          products && products.map((product) => <ProductCard key={product.id} product={product} />)
-        }
-      </div>
+      <Title>{category.toUpperCase()}</Title>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <CategoryContainer>
+          {products &&
+            products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+        </CategoryContainer>
+      )}
     </Fragment>
-
-  )
-}
+  );
+};
 
 export default Category;
